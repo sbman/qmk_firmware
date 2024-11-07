ce97cbf153230cce397f249c520f5ed95e3ee046
Scott <sbwinn@gmail.com>
2024-11-07 04:12:46 -0800

    Initial commit, incomplete setup.

---
+++ keyboards/split_programmer/keymaps/default/keymap.c
@@ -0,0 +1,29 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┐
+     * │ 7 │ 8 │ 9 │ / │
+     * ├───┼───┼───┼───┤
+     * │ 4 │ 5 │ 6 │ * │
+     * ├───┼───┼───┼───┤
+     * │ 1 │ 2 │ 3 │ - │
+     * ├───┼───┼───┼───┤
+     * │ 0 │ . │Ent│ + │
+     * └───┴───┴───┴───┘
+     */
+    [0] = LAYOUT(
+        KC_P7,  KC_P8,  KC_P9, KC_PSLS, 
+		KC_A, 	KC_B, 	KC_P4, KC_P5,   
+		KC_P6,  KC_PAST,KC_C,  KC_D,
+        KC_P1,  KC_P2,  KC_P3, KC_PMNS, 
+		
+		KC_E, 	KC_F,   KC_P0, KC_PDOT, 
+		KC_PENT,KC_PPLS,KC_G,  KC_H,
+		KC_P6,  KC_PAST,KC_C,  KC_D,
+        KC_P1,  KC_P2,  KC_P3, KC_PMNS
+    )
+};
